203828c60b80354bd58a43a4b07bd7a277930b65
Manuel <mfernandezortiz6@gmail.com>
2024-10-24 19:41:17 -0500

    Fix: Correciones de idioma y datos

--- src/app/charts/Earnings/data.ts
+++ src/app/charts/Earnings/data.ts
@@ -1,17 +1,17 @@
 'use client'
-import { faker } from '@faker-js/faker'
 import { useColors } from '../colors'
 import type { IMode } from './index'
+import { data } from './dataReal'
 
 const labelSets = {
   año: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
   mes: ['semana 1', 'semana 2', 'semana 3', 'semana 4'],
-  semana: ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+  semana: ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'],
   personalizado: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
 }
 
 const generateData = (labels: string[], min: number, max: number) =>
-  labels.map(() => faker.number.int({ min, max }) * 2500)
+  labels.map(() => data.map((item: any) => item.transaction_amount.influencer))
 
 export function useData (mode: IMode) {
   const { backgroundColor, borderColor } = useColors()
